9548c39e63d76badd0c49877ca94a8196e311cdf
Giacomo Pozzoni <giacomopoz@gmail.com>
2019-07-04 12:49:49 +0200

    Add test project to try reproducing an exception on swedish pcs

---
+++ Core/CoreResourcesTester/Program.cs
@@ -0,0 +1,23 @@
+﻿using OfficeDevPnP.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Globalization;
+
+namespace CoreResourcesTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread.CurrentThread.CurrentCulture = new CultureInfo("sv-se");
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("sv-se");
+            string resourceValue = GetCoreResources.FileFolderExtensions_CreateFolder0Under12;
+            Console.WriteLine("Resource value: " + resourceValue);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Core/OfficeDevPnP.Core/GetCoreResources.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core
+{
+    public class GetCoreResources
+    {
+        public static string FileFolderExtensions_CreateFolder0Under12
+        {
+            get
+            {
+                return CoreResources.FileFolderExtensions_CreateFolder0Under12;
+            }
+        }
+    }
+}
